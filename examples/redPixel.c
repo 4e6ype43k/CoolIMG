6111f72861619025e81ec24544c3e9f971183fa2
4e6ype43k <zelevan@outlook.com>
2026-04-10 18:16:16 +0100

    please stop

--- examples/redPixel.c
+++ examples/redPixel.c
@@ -1,1 +1,13 @@
-//! THIS IS NOT THE 
+//! THIS IS NOT THE FILE YOU SHOULD INCLUDE
+//! THIS FILE IS FOR DEMONSTRATIONAL PURPOSES ONLY
+
+#include "../src/coolIMG.h"
+
+int main() {
+    PixelData red;
+    red.width=1;
+    red.height=1;
+    allocPixelMemory(&red);
+    red.pixels[0]=(Color) {255,0,0,255};
+    encodePixelData(red,"red.cmig");
+}
